44e065e9f88d753ea2ebf630cd6b678288ecba60
Badhusa <dheenbadhush95@gmail.com>
2021-07-19 05:16:15 +0530

    Update script.js

--- Module4_Assignment/script.js
+++ Module4_Assignment/script.js
@@ -80,12 +80,6 @@ console.log(names);
 //   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
 //   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
 //   // name in the loop.
-//   if ( firstLetter == J)  {
-// //     byeSpeaker.sayBye();
-// //     // byeSpeaker.xxxx
-// //   } else {
-// //     helloSpeaker.sayHello();
-// //     // helloSpeaker.xxxx
-// //   }
-}
+
+// }
 // firstLetter();
